7ff024902480ff7dc8919fa8539c08b183edb343
Bobby Cox <bobby.cox@berkeley.edu>
2017-02-19 12:13:20 -0800

    more updates and added stringanimation

--- dna.py
+++ dna.py
@@ -3,18 +3,31 @@ from bibliopixel.led import *
 from random import randint
 from bibliopixel.colors import color_scale
 from bibliopixel.drivers.serial_driver import *
-from bibliopixel.animation import BaseStripAnim
+from bibliopixel.animation import BaseStripAnim, BaseMatrixAnim, AnimationQueue
+from bibliopixel.drivers.visualizer import DriverVisualizer
 import bibliopixel.gamma as gamma
+from bibliopixel.util import genVector
 from multiprocessing import Process, Value, Array
 import time
+
+# (1) Fade upwards to the top of the strand
+# (2) Jazz time
+# (3) Reverse of (1)
+
 
 #causes frame timing information to be output
 #bibliopixel.log.setLogLevel(bibliopixel.log.INFO)
 
-#Driver and Strip
-driver = DriverSerial(num=300, type=LEDTYPE.APA102, gamma=gamma.APA102)
+#Driver, Strip, and Matrix
+driver = DriverSerial(num=236, type=LEDTYPE.APA102)
+#visualizer = DriverVisualizer(width=2, height=118)
+
 strip = LEDStrip(driver, threadedUpdate=True, masterBrightness=150)
-matrix = LEDMatrix(width=2, height=150, driver, threadedUpdate=True, masterBrightness=150)
+# matrix = LEDMatrix(driver, 
+#                    width=2, 
+#                    height=118,
+#                    serpentine=False)
+queue = AnimationQueue(strip)
 
 #http://www.colourlovers.com/palette/1813691/Art_is_in_my_DNA
 cRed = (255,0,96)
@@ -52,98 +65,64 @@ class ColorFade(BaseStripAnim):
 
         self._step += amt
 
-
 class DNA(BaseStripAnim):
-    def __init__(self, led, start=0, end=-1):
-        super(DNA, self).__init__(led, start, end)
 
-        self._r = Array('i', self._led.numLEDs)
-        self._g = Array('i', self._led.numLEDs)
-        self._b = Array('i', self._led.numLEDs)
-        self._my_step = Value('i', 0) 
+    def wave_range(self, start, peak, step):
+        main = [i for i in range(start, peak+1, step)]
+        return main# + [i for i in reversed(main[0:len(main)-1])]
 
-        for i in range(self._led.numLEDs):
-            self._r[i] = 0
-            self._r[i] = 0
-            self._r[i] = 0
+    def __init__(self, led):
+        super(DNA, self).__init__(led)
+   
+        led.all_off()
 
-        p = Process(target=self.fillColor, args=(self._r, self._g, self._b, self._my_step))
-        p.start()
+        #Levels for brightness fade
+        self._levels = self.wave_range(30, 255, 70)
+     
+        #Base pair colors
+        self.colors = [cRed, cOrange, cBlue, cLime]
+        self._level_count = len(self._levels)
+        self._color_count = len(self.colors)
 
-    def step(self, amt = 1):
+        self._l_step = 0;
 
-        for i in range(self._led.numLEDs):
-            self._led.set(i, (self._r[i], self._g[i], self._b[i]))
 
-        self._step += amt
-        self._my_step.value = self._step
+    def step(self, amt=1):
 
-    def fillColor(self, r, g, b, step):
+        c_index = (self._step / self._level_count) % self._color_count
+        l_index = (self._l_step % self._level_count)
 
-        #Creates an array of levels to use for brightness values
-        def wave_range(self, start, peak, step):
-            main = [i for i in range(start, peak+1, step)]
-            return main + [i for i in reversed(main[0:len(main)-1])]
+        color = colors.color_scale(self.colors[(self._step) % self._color_count], self._levels[l_index])
+        
+        self._led.set(self._step, color)
 
-        self._levels = self.wave_range(30, 255, step)
+        self._l_step += 1;
+        #self._step += amt
+        if(self._l_step >= self._level_count):
+            self._l_step = 0
+            self._step += amt
 
-        left_strand = [cRed, cBlue]
-        right_strand = [cGreen, cOrange]
 
-        while True:
-            for i in range(0,150):
-                r[i], g[i], b[i] = my_colors[(step.value + i) % len(my_colors)]
-            time.sleep(.5)   
 
-    def fillColor2(self, r, g, b, step):
+from stringanimation import RainbowBottomUp
 
-        my_colors = [green, orange]
-
-        while True:
-            for i in range(200, 300):
-                r[i], g[i], b[i] = my_colors[(step.value + i) % len(my_colors)]
-            time.sleep(1)
-
-    def fillColor3(self, r, g, b, step):
-
-        while True:
-            for i in range(100):
-                r[i], g[i], b[i] = (0,0,0)
-            time.sleep(1)
-            for i in range(100):
-                r[i], g[i], b[i] = (255,255,255)
-            time.sleep(1)
-
-    def fillColor4(self, r, g, b, step):
-        i = 0
-        while True:
-            while i < 300:
-                r[i], g[i], b[i] = colors.Red
-                time.sleep(1)
-                i += 1
-
-#anim = RainbowCycle(strip)
-#anim = BottomUp(strip)
-#anim = RainbowBottomUp(strip)
-#anim = CrazyRainbowBottomUp(strip)
-#anim = LarsonScanner(led,color=(255,0,0), tail=25)
-#anim = Poops(led)
-#anim = MatrixPoops(led)
-#anim = MatrixCalibrationTest(led)
-#anim = RandMatrix(led)
-#anim = FireMatrix(led)
-
-#anim = DNA(strip)
+anim = DNA(strip)
 rainbow = [colors.Red, colors.Orange, colors.Yellow, colors.Green, colors.Blue, colors.Purple]
-anim = ColorFade(strip, rainbow)
+#anim = ColorFade(strip, rainbow, step=2)
+#anim2 = ColorFade(strip, rainbow, step=40)
+anim3 = RainbowBottomUp(strip)
+#anim = DNAMatrix(matrix)
+
+queue.addAnim(anim, max_steps=1180, fps=30)
+queue.addAnim(anim3)
 
 while True:
 
-    #try:
-    anim.run()
-    #except Exception as e:
-   #    print "problem: %s" % e
-    #except KeyboardInterrupt:
+    try:
+        queue.run()
+    except Exception as e:
+        print "problem: %s" % e
+    except KeyboardInterrupt:
         #Ctrl+C will exit the animation and turn the LEDs offs
-    #    strip.all_off()
-    #    break
+        strip.all_off()
+        break
